bdcf8f29a85d6a47d79a2691eb2b9b0cf850fc75
m3-welch <morganwelch@hotmail.com>
2020-10-22 19:03:21 +0100

    Forgot to push BankCustomer class

---
+++ Practical1/src/practical1/BankCustomer.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practical1;
+
+/**
+ *
+ * @author morgan
+ */
+public class BankCustomer {
+    private double balance;
+    private String username;
+    
+    public String getUsername() {
+        return this.username;
+    }
+    
+    public String setUsername(String username) {
+        this.username = username;
+        return this.username;
+    }
+    
+    public double getBalance() {
+        return this.balance;
+    }
+    
+    public double setBalance(double balance) {
+        this.balance = balance;
+        return this.balance;
+    }
+    
+    public double deposit(double amount) {
+        this.balance += amount;
+        return this.balance;
+    }
+    
+    public double withdraw(double amount) throws Exception {
+        if (this.balance - amount < 0) {
+            throw new Exception("You do not have the funds to withdraw that amount.");
+        }
+        this.balance -= amount;
+        return this.balance;
+    }
+    
+    public double getPercentage(double percentage) {
+        return this.balance / 100 * percentage;
+    }
+    
+}
