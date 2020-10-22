08980ebdaac026c26b2e45464ddb174d278481d9
Sam <kirkysam9@gmail.com>
2020-10-22 15:16:25 +0100

    Test

---
+++ Practical1/src/practical1/Triangle.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practical1;
+
+/**
+ *
+ * @author sam
+ */
+public class Triangle {
+    int sides = 3;
+    double lSide1;
+    double lSide2;
+    double lSide3;
+    
+    private double getSide1(){
+        return lSide1;
+    }
+    private void setSide1(double newL){
+        this.lSide1 = newL;
+    }
+    
+    private double getSide2(){
+        return lSide1;
+    }
+    private void setSide2(double newL){
+        this.lSide1 = newL;
+    }
+    
+    private double getSide3(){
+        return lSide1;
+    }
+    private void setSide3(double newL){
+        this.lSide1 = newL;
+    }
+    
+}
